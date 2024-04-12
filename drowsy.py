5b7f19f9c7d278687f14de107c41ee21a04b4fb0
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-04-13 00:07:15 +0530

    Update drowsy.py - Added sound

--- drowsy.py
+++ drowsy.py
@@ -11,24 +11,24 @@ def eye_aspect_ratio(eye):
     C = distance.euclidean(eye[0], eye[3])
     ear = (A + B) / (2.0 * C)
     return ear
-    
+
 thresh = 0.27
 frame_check = 18
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat") # Path to your shape_predictor_68_face_landmarks.dat file
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap=cv2.VideoCapture(0)
-flag=0
+cap = cv2.VideoCapture(0)
+flag = 0
 while True:
-    ret, frame=cap.read()
+    ret, frame = cap.read()
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     subjects = detect(gray, 0)
     for subject in subjects:
         shape = predict(gray, subject)
-        shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+        shape = face_utils.shape_to_np(shape)
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
@@ -40,13 +40,16 @@ while True:
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
         if ear < thresh:
             flag += 1
-            print (flag)
             if flag >= frame_check:
-                playsound("assets/beep.mp3")
-                cv2.putText(frame, "****************ALERT!****************", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "****************ALERT!****************", (10,325), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                #print ("Drowsy")
-                
+                if ear < 0.2:  # Closed completely - High alarm
+                    playsound("assets/digital-alarm-clock-151920.mp3")
+                    cv2.putText(frame, "Eyes Closed Completely!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                elif ear < 0.25:  # Partially closed - Medium alarm
+                    playsound("assets/beep.mp3")
+                    cv2.putText(frame, "Eyes Partially Closed!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                else:  # Little closed
+                    playsound("assets/beep-sound-8333.mp3")
+                    cv2.putText(frame, "Eyes Little Closed!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         else:
             flag = 0
     cv2.imshow("Frame", frame)
@@ -54,4 +57,4 @@ while True:
     if key == ord("q"):
         break
 cv2.destroyAllWindows()
-cap.release() 
+cap.release()
